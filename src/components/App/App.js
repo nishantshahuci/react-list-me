55d216baf451c91f60d39cfc66f6909c4e6a569d
Nishant Shah <nishantshahuci@gmail.com>
2019-03-09 18:45:13 -0500

    Refactor gradient to body

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Header from '../Header/Header';
+import Login from '../Login/Login';
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
       <Header />
+      <Login />
     </Fragment>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,17 +5,15 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__text-box">
-        <h1 className="header-text">
-          <span className="header-text__main">Welcome to ListMe</span>
-          <span className="header-text__sub">
-            Your first step to organization
-          </span>
-        </h1>
-        <a href="/" className="header__button">
-          <span className="header__button-text">Get started</span>
-        </a>
-      </div>
+      <h1 className="header-text">
+        <span className="header-text__main">Welcome to ListMe</span>
+        <span className="header-text__sub">
+          Your first step to organization
+        </span>
+      </h1>
+      <a href="/" className="header__button">
+        <span className="header__button-text">Get started</span>
+      </a>
     </div>
   );
 };
