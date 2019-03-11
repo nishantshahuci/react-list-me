a72be20a83d3ca00717e1638f6a7dafdd3fdda18
Nishant Shah <nishantshahuci@gmail.com>
2019-03-11 17:05:04 -0400

    Update button styling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Button = ({ btnClasses, btnText, btnTextClasses }) => {
   return (
-    <a className={btnClasses}>
+    <div className={btnClasses}>
       <span className={btnTextClasses}>{btnText}</span>
-    </a>
+    </div>
   );
 };
 
