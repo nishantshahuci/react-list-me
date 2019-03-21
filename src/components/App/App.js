ec9f8c5598fbb25243e2ab5fee9105b3eac74722
Nishant Shah <nishantshahuci@gmail.com>
2019-03-21 13:54:16 -0400

    Set up redux, thunk, and react-router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,48 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
 import Navbar from '../Navbar/Navbar';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Dashboard from '../Dashboard/Dashboard';
 import List from '../List/List';
+import history from '../../history';
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      {/* <Dashboard
-        lists={[
-          {
-            id: 1,
-            title: 'Groceries',
-            items: [
-              { id: 1, title: 'Milk', complete: false },
-              { id: 2, title: 'Eggs', complete: false },
-              { id: 3, title: 'Cheese', complete: true }
-            ]
-          },
-          {
-            id: 2,
-            title: 'Chores',
-            items: [
-              { id: 1, title: 'Clean bathroom', complete: false },
-              { id: 2, title: 'Wash dishes', complete: false },
-              { id: 3, title: 'Do laundry', complete: true }
-            ]
-          }
-        ]}
-      /> */}
-      <List
-        list={{
-          id: 2,
-          title: 'Chores',
-          items: [
-            { id: 1, title: 'Clean bathroom', complete: false },
-            { id: 2, title: 'Wash dishes', complete: false },
-            { id: 3, title: 'Do laundry', complete: true }
-          ]
-        }}
-      />
-    </div>
+    <Router history={history}>
+      <div className="app">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Header} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/list/:id" exact component={List} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
 import App from './components/App/App';
 
 import './index.scss';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
